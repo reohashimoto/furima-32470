90ee2731008527b9d85d3736bc34d013fb939d0c
reohashimoto <doribler@gmail.com>
2020-12-18 18:12:45 +0900

    ログイン機能実装

---
+++ db/migrate/20201218070330_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :product_name, null: false
+      t.integer :item_category_id, null: false
+      t.integer :price, null: false
+      t.integer :delivery_fee_id, null: false
+      t.integer :shipping_area_id, null: false
+      t.integer :days_to_ship_id, null: false
+      t.text :item_explanation, null: false
+      t.integer :status_id, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
