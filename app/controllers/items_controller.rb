20e3b7ea4039980e44e536594a886c0b0e4b6a9d
reohashimoto <doribler@gmail.com>
2020-12-21 23:06:32 +0900

    activeHashの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,12 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-  #def new
-    #@item = Item.new
-  #end
-
+  def new
+    @item = Item.new
+  end
+  def create
+    Item.create(item_params)
+  end
   def edit
   end
 
@@ -15,6 +17,9 @@ class ItemsController < ApplicationController
   end
 
   private
+  def item_params
+    params.require(:item).permit(:product_name, :item_category_id, :price, :delivary_fee_id, :shipping_area_id, :days_to_ship_id, :item_explanation).merge(user_id: current_user.id)
+  end
  def set_item
   @item = Item.find(params[:id])
  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item < ApplicationRecord
-  #validates :product_name, presence: true
-  #validates :item_category_id, presence: true
-  #validates :price, presence: true
-  #validates :delivary_fee_id, presence: true
-  #validates :shipping_area_id, presence: true
-  #validates :days_to_ship_id, presence: true
-  #validates :item_explanation, presence: true
-  #belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :product_condition
+  belongs_to :delivery_fee
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+  validates :product_name
+  validates :item_category_id, numericality: { order_than: 1 }
+  validates :product_condition_id
+  validates :price
+  validates :delivery_fee_id
+  validates :shipping_area_id
+  validates :days_to_ship_id
+  validates :item_explanation
+  end
+  belongs_to :user
 end
