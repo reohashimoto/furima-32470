6e78a1a3b257e7a853a5e57bbed3feeab2d02861
reohashimoto <doribler@gmail.com>
2021-01-04 16:11:00 +0900

    修正４

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
     validates_inclusion_of :price, in: 300..9999999
     validates :item_explanation
     validates :image
-      validates :item_category_id, numericality: { order_than: 1 }
-      validates :product_condition_id, numericality: { order_than: 1 }
-      validates :delivery_fee_id, numericality: { order_than: 1 }
-      validates :shipping_area_id, numericality: { order_than: 1 }
-      validates :days_to_ship_id, numericality: { order_than: 1 }
+      validates :item_category_id, numericality: { other_than: 1, message: "Select" }
+      validates :product_condition_id, numericality: { other_than: 1, message: "Select" }
+      validates :delivery_fee_id, numericality: { other_than: 1, message: "Select" }
+      validates :shipping_area_id, numericality: { other_than: 1, message: "Select" }
+      validates :days_to_ship_id, numericality: { other_than: 1, message: "Select" }
   end
   belongs_to :user
   has_one_attached :image
