4d2c3ee9cca7b9521aa9447c0182bcc5c2f8eace
reohashimoto <doribler@gmail.com>
2021-01-04 12:57:03 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :destroy]
+
   def index
      @items = Item.all.order("created_at DESC")
   end
@@ -9,6 +10,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,17 +18,17 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-
   end
 
   def edit
-    if @item != nil
+    if @item.purchase != nil 
       redirect_to root_path
     end
   end
 
   def show
   end
+
   def update
     if @item.update(item_params)
       redirect_to item_path
@@ -39,16 +41,20 @@ class ItemsController < ApplicationController
     @item.destroy
     redirect_to root_path
   end
+
   private
+
   def item_params
     params.require(:item).permit(:product_name, :product_condition_id, :item_category_id, :price, :delivery_fee_id, :shipping_area_id, :days_to_ship_id, :item_explanation, :image).merge(user_id: current_user.id)
   end
- def set_item
-  @item = Item.find(params[:id])
- end
- def move_to_index
-  unless current_user.id == @item.user.id
-    redirect_to action: :index
+
+  def set_item
+    @item = Item.find(params[:id])
   end
-end
+  
+  def move_to_index
+    if current_user.id != @item.user_id
+      redirect_to action: :index
+    end
+  end
 end
